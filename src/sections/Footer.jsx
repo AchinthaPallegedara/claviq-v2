8d6b98e7f1ec4bd252d89119f1c613d515bd8f88
Achintha Pallegedara <achinthahirudika@gmail.com>
2024-05-31 01:49:50 +0530

    design footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -6,16 +5,54 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const Footer = () => {
-  useGSAP(() => {}, []);
+  const t1 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#footerSection",
+      start: "top 50%",
+      end: "bottom 80%",
+      scrub: true,
+    },
+  });
+  useGSAP(() => {
+    t1.to("#footerSection", {
+      backgroundColor: "#000",
+
+      duration: 1,
+      delay: 2,
+    });
+    t1.fromTo(
+      "#footerTitle",
+      {
+        y: 100,
+        duration: 1,
+      },
+      {
+        y: -60,
+        duration: 1,
+      }
+    );
+    t1.to("#workText", {
+      rotate: 10,
+
+      duration: 1,
+      delay: 2,
+    });
+  }, []);
 
   return (
-    <section className="mt-[200vh] flex items-center w-full h-[100vh] justify-center bg-black">
-      <h3 className="">
-        <span className="relative text-[12vw] font-normal text-center text-white  underline-animation">
-          Let's Work
+    <section
+      className="mt-[200vh] flex items-center w-full h-[100vh] justify-center text-white "
+      id="footerSection"
+    >
+      <h3
+        className="text-[12vw] font-normal text-center   underline-offset-3 transition-all duration-300 ease-in-out "
+        id="footerTitle"
+      >
+        <span className="">
+          Let's <span id="workText">Work</span>
         </span>
         <br />
-        <span className="relative text-[12vw] font-normal text-center text-white  underline-animation">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-main-300 via-main-500 to-main-100 ">
           Together
         </span>
       </h3>
